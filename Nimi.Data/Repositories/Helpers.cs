e134c9c97309732186fde9c22b7bad02656c5ad7
orion787 <116973576+orion787@users.noreply.github.com>
2025-05-31 12:19:26 +0300

    Create Helpers.cs

---
+++ Nimi.Data/Repositories/Helpers.cs
@@ -0,0 +1,31 @@
+namespace Nimi.Data.Repositories.Helpers
+{
+    internal static class SolutionPathHelper
+    {
+        public static string GetSolutionDirectory()
+        {
+            // Начинаем с директории приложения
+            var directory = new DirectoryInfo(AppContext.BaseDirectory);
+
+            // Поднимаемся вверх по иерархии папок, пока не найдем файл .sln
+            while (directory != null && !DirectoryContainsSolution(directory))
+            {
+                directory = directory.Parent;
+            }
+
+            return directory?.FullName
+                ?? throw new InvalidOperationException("Solution directory not found!");
+        }
+
+        private static bool DirectoryContainsSolution(DirectoryInfo directory)
+        {
+            return directory.GetFiles("*.sln").Length > 0;
+        }
+    }
+
+    public static class Helper
+    {
+        private static readonly string _solutionPath = SolutionPathHelper.GetSolutionDirectory();
+        public static string DbPath = Path.Combine(_solutionPath, "Resources", "partners.db");
+    }
+}
