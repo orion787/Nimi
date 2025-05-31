b7394c66fd1a759515babfbb94655635a6867acf
orion787 <116973576+orion787@users.noreply.github.com>
2025-05-31 12:20:44 +0300

    Update Repository.cs

--- Nimi.Data/Repositories/Repository.cs
+++ Nimi.Data/Repositories/Repository.cs
@@ -1,33 +1,36 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Nimi.Core.Entities;
 using Nimi.Data.DbContexts;
 
-namespace Nimi.Data.Repositories
+namespace Nimi.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+public class Repository<T> : INimiRepository<T> where T : EntityBase
 {
-    public class Repository<T> : INimiRepository<T> where T : EntityBase
+    private readonly NimiDbContext _ctx;
+    private readonly DbSet<T> _dbSet;
+
+    public Repository(NimiDbContext ctx)
     {
-        private readonly NimiDbContext _ctx;
-        private readonly DbSet<T> _dbSet;
-
-        public Repository(NimiDbContext ctx)
-        {
-            _ctx = ctx;
-            _dbSet = _ctx.Set<T>();
-        }
-
-        public IEnumerable<T> GetAll()
-            => _dbSet.AsNoTracking().ToList();
-
-        public T? GetById(int id)
-            => _dbSet.Find(id);
-
-        public void Add(T entity)
-            => _dbSet.Add(entity);
-
-        public void Update(T entity)
-            => _dbSet.Update(entity);
-
-        public void Remove(T entity)
-            => _dbSet.Remove(entity);
+        _ctx = ctx;
+        _dbSet = _ctx.Set<T>();
     }
+
+    public IQueryable<T> Query()
+        => _dbSet.AsQueryable();
+
+    public IEnumerable<T> GetAll()
+        => _dbSet.AsNoTracking().ToList();
+
+    public T? GetById(int id)
+        => _dbSet.Find(id);
+
+    public void Add(T entity)
+        => _dbSet.Add(entity);
+
+    public void Update(T entity)
+        => _dbSet.Update(entity);
+
+    public void Remove(T entity)
+        => _dbSet.Remove(entity);
 }
