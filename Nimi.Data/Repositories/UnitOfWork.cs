112d5a50bd90c633664357cd5ba09203266b7ec1
orion787 <116973576+orion787@users.noreply.github.com>
2025-05-31 12:21:17 +0300

    Update UnitOfWork.cs

--- Nimi.Data/Repositories/UnitOfWork.cs
+++ Nimi.Data/Repositories/UnitOfWork.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore;
 using Nimi.Data.DbContexts;
 using Nimi.Data.Models;
